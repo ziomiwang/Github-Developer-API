8518e6e50e87b9f92c7e7139ee290ff9884f8f37
ziomiwang <ziomiwang@gmail.com>
2024-02-13 23:47:36 +0100

    added swaggerv3 for API documentation

--- src/main/java/org/example/githubdeveloperapi/GithubDeveloperApiApplication.java
+++ src/main/java/org/example/githubdeveloperapi/GithubDeveloperApiApplication.java
@@ -3,9 +3,6 @@ package org.example.githubdeveloperapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 @SpringBootApplication
 public class GithubDeveloperApiApplication {
--- src/main/java/org/example/githubdeveloperapi/web/GithubController.java
+++ src/main/java/org/example/githubdeveloperapi/web/GithubController.java
@@ -1,21 +1,28 @@
 package org.example.githubdeveloperapi.web;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.example.githubdeveloperapi.model.GithubRepository;
 import org.example.githubdeveloperapi.service.GithubService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@Tag(name = "Github", description = "github repositories API")
+@RestController
 @RequiredArgsConstructor
 public class GithubController {
 
     private final GithubService githubService;
 
+    @Operation(
+            summary = "Fetch all github repositories",
+            description = "Fetches all github repositories and their branches for provided username"
+    )
     @GetMapping("/repos")
     public ResponseEntity<List<GithubRepository>> getUserRepos(@RequestParam("username") String username) {
         return ResponseEntity.ok(githubService.fetchUserRepos(username));
